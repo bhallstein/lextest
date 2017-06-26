3f5bf634e71eed912d09ea610c0924feed803d7c
Ben <ben@Fratercula.local>
2017-06-27 00:03:58 +0100

    Non-substr version

--- lex.js
+++ lex.js
@@ -2,9 +2,9 @@
 const tokens = {
 	BOOL_TRUE : 'BOOL_TRUE',
 	BOOL_FALSE : 'BOOL_FALSE',
-	
+
 	ID : 'ID',
-	
+
 	WHITESPACE : 'WHITESPACE',
 	END_OF_INPUT : 'END_OF_INPUT',
 };
@@ -12,7 +12,7 @@ const tokens = {
 function mk_token(which, value) {
 	return { which, value };
 }
-	
+
 const errors = {
 	NO_MATCHING_PATTERN : 'NO_MATCHING_PATTERN',
 	AMBIGUOUS_GRAMMAR : 'AMBIGUOUS_GRAMMAR',
@@ -25,8 +25,8 @@ const patterns = [
 	[ /^\s+/, tokens.WHITESPACE ],
 ];
 
-function get_match(input) {
-	if (input.length === 0) {
+function get_match(input, i) {
+	if (i >= input.length - 1) {
 		return tokens.END_OF_INPUT;
 	}
 
@@ -34,53 +34,56 @@ function get_match(input) {
 		.map(function(s) {
 			const regex = s[0];
 			const token = s[1];
-		
+
 			var result = input.match(regex);
 			if (result !== null) {
 				result = result[0];
 			}
-		
+
 			return { token, result };
 		})
 		.filter((r) => (r.result !== null))
 		.sort((a, b) => b.result.length - a.result.length);
-	
+
 	if (results.length === 0) {
 		return errors.NO_MATCHING_PATTERN;
 	}
-	
+
 	return mk_token(results[0]['token'], results[0]['result']);
 }
 
 
 function lex(input) {
 	var error = false;
-	
+	var i = 0;
+
 	var out = [ ];
 	function emit(t) { out.push(t); }
-	
+
 	while (true) {
-		var m = get_match(input);
-		
+		var m = get_match(input, i);
+
 		if (m === tokens.END_OF_INPUT) {
 			break;
 		}
-		
+
 		if (errors[m] === m) {
 			console.log("Error: ", m, "(@'" + input.substr(0,20) + "...')");
 			error = true;
 			break;
 		}
-		
-		input = input.substr(m.value.length);
-		
+
+		i += m.value.length;
+		// console.log(i);
+		// input = input.substr(m.value.length);
+
 		if (m.which === tokens.WHITESPACE) {
 			continue;
 		}
-		
+
 		emit(m);
 	}
-	
+
 	return error ? null : out;
 }
 
@@ -88,5 +91,4 @@ var s = Array(100000).join('false falseyness  true ');
 
 var parse_result = lex(s);
 // console.log(parse_result);
-
 
